fe2216b1da67fd51e8db173b19c33cc814e0338a
Yabacoder <yabacoder@gmail.com>
2021-03-22 14:25:24 +0100

    fixed styles

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+ function toggleMenu() {
+        const menuToggle = document.querySelector('.toggle');
+        const navigation = document.querySelector('.navigation');
+        
+        menuToggle.classList.toggle('active');
+        navigation.classList.toggle('active');
+    }
